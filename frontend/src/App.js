a84bba91cbcc24627d40e84d5b97fa8692a72514
yawtalkstechs <yawtalkstechs@gmail.com>
2022-03-29 13:29:04 +0000

    Frontend updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,37 @@
-import { render } from '@testing-library/react';
 import React, { Component } from 'react';
-
-const list = [
-  {"id":1,"title":"Learn Tailwind CSS","body":"- Learn containers\r\n- Learn Flexbox\r\n- Learn Grid\r\n- Learn how to create a nav-bar"},
-  {"id":2,"title":"Amazon S3 buckets","body":"- Create an AWS s3 buckets\r\n- Connect django app to the buckets"},
-  {"id":3,"title":"Revise on ML","body":"- Revise on Supervised Learning.\r\n- Revise on unsupervised Learning."}]
+import axios from 'axios';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = { list };
+  state = {
+    todos:[]
+  };
+  
+  componentDidMount() {
+    this.getTodos();
   }
+
+  getTodos() {
+    axios
+      .get('http://127.0.0.1:8000/api/')
+      .then(res => {
+        this.setState({ todos: res.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
   render(){
     return (
-      <div class="justify-content:center">
-        {this.state.list.map(item => (
+      <div>
+        {this.state.todos.map(item => (
           <div key={item.id}>
             <h1>{item.title}</h1>
-            <p>{item.body}</p>
+            <span>{item.body}</span>
           </div>
         ))}
       </div>
-    )
+    );
   }
 }
 
